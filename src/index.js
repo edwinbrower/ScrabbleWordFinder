099dfa50fa7e19e3e8e7c5ac4139466fafa0e838
Edwin Brower <edwin.brower@gmail.com>
2017-11-02 01:17:13 -0700

    Append results to page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const input = document.querySelector('form');
-
+const results = document.querySelector('.results');
 
 const processInputWord = e => {
   e.preventDefault();
@@ -10,19 +10,24 @@ const processInputWord = e => {
     window.alert(`Enter no more than 7 letters! Your input '${word}' contains ${word.length} letters!`);
   } else {
     console.log(word);
-    // POST to server
     $.ajax({
       url: '/permutations', 
       type: 'POST',
       data: word,
       success: (data) => {
         console.log('success', data);
+        results.innerHTML = `<h6>There are ${data.length} valid words.</h6>`;
+        data.forEach(word => {
+          let item = document.createElement('P');
+          let text = document.createTextNode(word);
+          item.appendChild(text);
+          results.appendChild(item);
+        });
       },
       error: (err) => {
         console.log('err', err);
       }
     });
-    // add words to page 
   }
 };
 

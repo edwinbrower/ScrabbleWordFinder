bebf7f9284eb110f7cc02f1f88b6df4b6706d1d8
Edwin Brower <edwin.brower@gmail.com>
2017-11-01 21:40:27 -0700

    connect server to client

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'src')));
+
 const fs = require('fs');
 const dictionary = fs.readFileSync('./dictionary.txt', 'utf-8');
 const dictionaryAray = dictionary.split('\n');
@@ -38,11 +49,29 @@ const findAllPermutationsAllLength = string => {
   return Object.keys(result);  
 };
 
-const result = [];
-findAllPermutationsAllLength('byeman').forEach(perm => {
-  if (binarySearch(dictionaryAray, perm)) {
-    result.push(perm);
-  }
+// const result = [];
+// findAllPermutationsAllLength('byeman').forEach(perm => {
+//   if (binarySearch(dictionaryAray, perm)) {
+//     result.push(perm);
+//   }
+// });
+
+// console.log(result);
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
 });
 
-console.log(result);
+app.post('/permutations', function(req, res) {
+  const result = [];
+  // findAllPermutationsAllLength('byeman').forEach(perm => {
+  //   if (binarySearch(dictionaryAray, perm)) {
+  //     result.push(perm);
+  //   }
+  // });
+  console.log('posted');
+});
+
+app.listen(3001, function() {
+  console.log('listening on port 3001!');
+});
